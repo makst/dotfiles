e4227d1a581c7ddbfe7dad76169b282c30896e90
Max Tkachenko <makst@users.noreply.github.com>
2022-09-11 14:37:19 +0200

    feat(fzf): default opts"

--- .fzf.zsh
+++ .fzf.zsh
@@ -3,6 +3,24 @@
 # hence, only `brew install fzf`, do not manually source auto-completion and key bindings
 source ~/.oh-my-zsh/plugins/fzf/fzf.plugin.zsh
 
+### fzf configuration
+bindkey "ç" fzf-cd-widget
+
+# inspired by: https://betterprogramming.pub/boost-your-command-line-productivity-with-fuzzy-finder-985aa162ba5d
+export FZF_DEFAULT_OPTS="
+--multi
+--preview-window=:hidden
+--color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008'
+--preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+--bind '?:toggle-preview'
+--bind 'ctrl-a:select-all'
+--bind 'ctrl-y:execute-silent(echo {+} | pbcopy)'
+--bind 'ctrl-n:preview-down'
+--bind 'ctrl-p:preview-up'
+--bind 'ctrl-alt-n:preview-half-page-down'
+--bind 'ctrl-alt-p:preview-half-page-up'
+"
+
 ###### fzf-tab configuration - https://github.com/Aloxaf/fzf-tab#configure
 # disable sort when completing `git checkout`
 zstyle ':completion:*:git-checkout:*' sort false
