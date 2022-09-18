4b8d211280d46f361513ce220a520f2244fe11d6
Max Tkachenko <makst@users.noreply.github.com>
2022-09-18 20:13:06 +0200

    feat(nvim-tree): update key bindings

--- config/nvim/after/plugin/nvim-tree.lua
+++ config/nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,16 @@
 local status, nvim_tree = pcall(require, "nvim-tree")
 if (not status) then return end
 
-nvim_tree.setup {}
+nvim_tree.setup {
+  view = {
+    mappings = {
+      list = {
+        { key = "l", action = "edit" }
+      }
+    }
+  }
+}
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<leader>e', '<Cmd>NvimTreeToggle<CR>', opts)
+vim.keymap.set('n', '<leader>f', '<Cmd>NvimTreeFindFileToggle<CR>', opts)
