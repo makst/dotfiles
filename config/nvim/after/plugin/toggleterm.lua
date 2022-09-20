ca20f4dfeacd722921f4c609da42feb5668eb09b
Max Tkachenko <makst@users.noreply.github.com>
2022-09-20 18:26:53 +0200

    feat(term): terminals

--- config/nvim/after/plugin/toggleterm.lua
+++ config/nvim/after/plugin/toggleterm.lua
@@ -18,4 +18,33 @@ function _lazygit_toggle()
   lazygit:toggle()
 end
 
+local testterm = Terminal:new({
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+})
+
+local miscterm = Terminal:new({
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+})
+
+function _testterm_toggle()
+  testterm:toggle()
+end
+
+function _miscterm_toggle()
+  miscterm:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tt", "<cmd>lua _testterm_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<leader>tt", "<cmd>lua _testterm_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tm", "<cmd>lua _miscterm_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<leader>tm", "<cmd>lua _miscterm_toggle()<CR>", { noremap = true, silent = true })
