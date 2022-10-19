83335abc40c6e8135401c2f19f00263f40b320df
Max Tkachenko <makst@users.noreply.github.com>
2022-10-19 23:47:41 +0200

    feat(nvim): rearrange lua files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,3 @@
 require('base')
-require('highlights')
-require('maps')
-require('plugins')
+require('keymaps')
+require('user.plugins')
--- config/nvim/lua/base.lua
+++ config/nvim/lua/base.lua
@@ -30,6 +30,13 @@ vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
 
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
+
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = '*',
