1c16264c00b9a8076a6ee693e23d616512e625d3
Max Tkachenko <makst@users.noreply.github.com>
2022-10-22 17:46:57 +0200

    refactor(nvim): trouble

--- config/nvim/lua/user/lsp/init.lua
+++ config/nvim/lua/user/lsp/init.lua
@@ -1,3 +1,4 @@
 require('user.lsp.lspconfig')
 require('user.lsp.lspkind')
 require('user.lsp.lspsaga')
+require('user.lsp.trouble')
---
+++ config/nvim/lua/user/lsp/trouble/init.lua
@@ -0,0 +1,2 @@
+require('user.lsp.trouble.config')
+require('user.lsp.trouble.keymaps')
