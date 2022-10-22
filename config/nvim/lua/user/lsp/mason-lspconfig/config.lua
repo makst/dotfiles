b246a9b2c446618420b944241739ddd689a80f81
Max Tkachenko <makst@users.noreply.github.com>
2022-10-23 00:27:21 +0200

    feat(nvim): yamlls language server

--- config/nvim/lua/user/lsp/mason-lspconfig/config.lua
+++ config/nvim/lua/user/lsp/mason-lspconfig/config.lua
@@ -1,6 +1,9 @@
-local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+---@diagnostic disable-next-line: unused-local
+local status_ok, _mason_lspconfig = pcall(require, "mason-lspconfig")
 if not status_ok then return end
 
+local mason_lspconfig = require("mason-lspconfig")
+
 mason_lspconfig.setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "eslint" }
+  ensure_installed = { "sumneko_lua", "tsserver", "eslint", "yamlls" }
 })
