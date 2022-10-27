6067e6a6ebe733831e47c04f5d6f8262d4706615
Max Tkachenko <makst@users.noreply.github.com>
2022-10-28 00:20:15 +0200

    feat(nvim): toggleterm keymaps

--- config/nvim/lua/user/toggleterm/config.lua
+++ config/nvim/lua/user/toggleterm/config.lua
@@ -4,41 +4,55 @@ if (not status) then return end
 
 toggleterm.setup {}
 
+local lg_terminal_map = {}
+local test_terminal_map = {}
+local misc_terminal_map = {}
+
 local Terminal = require('toggleterm.terminal').Terminal
 
-local lazygit = Terminal:new({
-  cmd = "lazygit",
-  hidden = true,
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-})
-
-local testterm = Terminal:new({
-  hidden = true,
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-})
-
-local miscterm = Terminal:new({
-  hidden = true,
-  direction = "float",
-  float_opts = {
-    border = "double",
-  },
-})
-
 function _lazygit_toggle()
-  lazygit:toggle()
+  local cwd = vim.fn.getcwd()
+  if (lg_terminal_map[cwd] == nil) then
+    lg_terminal_map[cwd] = Terminal:new({
+      cmd = "lazygit",
+      autochdir = true,
+      hidden = true,
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+    })
+  end
+
+  lg_terminal_map[cwd]:toggle()
 end
 
 function _testterm_toggle()
-  testterm:toggle()
+  local cwd = vim.fn.getcwd()
+  if (test_terminal_map[cwd] == nil) then
+    test_terminal_map[cwd] = Terminal:new({
+      hidden = true,
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+    })
+  end
+
+  test_terminal_map[cwd]:toggle()
 end
 
 function _miscterm_toggle()
-  miscterm:toggle()
+  local cwd = vim.fn.getcwd()
+  if (misc_terminal_map[cwd] == nil) then
+    misc_terminal_map[cwd] = Terminal:new({
+      hidden = true,
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+    })
+  end
+
+  misc_terminal_map[cwd]:toggle()
 end
