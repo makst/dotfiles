2aa1984e80880675858622c3499779aa63d223fd
Max Tkachenko <makst@users.noreply.github.com>
2022-10-27 12:03:09 +0200

    refactor(nvim): lsp icons

--- config/nvim/lua/user/lsp/lspconfig/config.lua
+++ config/nvim/lua/user/lsp/lspconfig/config.lua
@@ -1,8 +1,6 @@
--- this is for diagnositcs signs on the line number column
--- use this to beautify the plain E W signs to more fun ones
--- !important nerdfonts needs to be setup for this to work in your terminal
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-for type, icon in pairs(signs) do
+local icons_module = require("user.lsp.lspconfig.icons")
+
+for type, icon in pairs(icons_module.icons) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
@@ -11,7 +9,7 @@ vim.diagnostic.config({
   virtual_lines = false,
   virtual_text = false,
   signs = {
-    active = signs,
+    active = icons_module.icons,
   },
   float = {
     border = "rounded",
---
+++ config/nvim/lua/user/lsp/lspconfig/icons.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+-- this is for diagnositcs signs on the line number column
+-- use this to beautify the plain E W signs to more fun ones
+-- !important nerdfonts needs to be setup for this to work in your terminal
+M.icons = {
+  Error = " ",
+  Warn = " ",
+  Hint = " ",
+  Info = " "
+}
+
+return M
