82b2b5be3a7a2df364d96302bb193a0343788d63
Max Tkachenko <makst@users.noreply.github.com>
2022-10-27 23:33:28 +0200

    feat(nvim): nvim-tree config

--- config/nvim/lua/user/nvim-tree/config.lua
+++ config/nvim/lua/user/nvim-tree/config.lua
@@ -2,6 +2,10 @@ local status, nvim_tree = pcall(require, "nvim-tree")
 if (not status) then return end
 
 nvim_tree.setup {
+  sync_root_with_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
   view = {
     adaptive_size = true,
     mappings = {
