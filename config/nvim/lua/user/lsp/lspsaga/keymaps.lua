c243d46a177fc2e4bce43155e3b1f56eff0a7487
Max Tkachenko <makst@users.noreply.github.com>
2022-10-29 14:43:00 +0200

    feat(nvim): lsp keymaps

--- config/nvim/lua/user/lsp/lspsaga/keymaps.lua
+++ config/nvim/lua/user/lsp/lspsaga/keymaps.lua
@@ -1,6 +1,6 @@
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
-vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+vim.keymap.set('n', '<leader>hd', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gre', '<Cmd>Lspsaga rename<CR>', opts)
--- config/nvim/lua/user/tree-climber/keymaps.lua
+++ config/nvim/lua/user/tree-climber/keymaps.lua
@@ -6,4 +6,4 @@ vim.keymap.set({ 'n', 'v', 'o' }, 'K', require('tree-climber').goto_prev, keyopt
 vim.keymap.set({ 'v', 'o' }, 'in', require('tree-climber').select_node, keyopts)
 vim.keymap.set('n', '<leader>k', require('tree-climber').swap_prev, keyopts)
 vim.keymap.set('n', '<leader>j', require('tree-climber').swap_next, keyopts)
-vim.keymap.set('n', '<leader>h', require('tree-climber').highlight_node, keyopts)
+vim.keymap.set('n', '<leader>hn', require('tree-climber').highlight_node, keyopts)
