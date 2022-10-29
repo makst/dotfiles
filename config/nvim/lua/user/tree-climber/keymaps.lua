67efeb844164739574584edec98b451d4b8adae2
Max Tkachenko <makst@users.noreply.github.com>
2022-10-29 12:26:59 +0200

    feat(nvim): tree-climber keymaps update

--- config/nvim/lua/user/tree-climber/keymaps.lua
+++ config/nvim/lua/user/tree-climber/keymaps.lua
@@ -4,7 +4,6 @@ vim.keymap.set({ 'n', 'v', 'o' }, 'L', require('tree-climber').goto_child, keyop
 vim.keymap.set({ 'n', 'v', 'o' }, 'J', require('tree-climber').goto_next, keyopts)
 vim.keymap.set({ 'n', 'v', 'o' }, 'K', require('tree-climber').goto_prev, keyopts)
 vim.keymap.set({ 'v', 'o' }, 'in', require('tree-climber').select_node, keyopts)
-vim.keymap.set('n', '<c-k>', require('tree-climber').swap_prev, keyopts)
-vim.keymap.set('n', '<c-j>', require('tree-climber').swap_next, keyopts)
-vim.keymap.set('n', '<c-h>', require('tree-climber').highlight_node, keyopts)
-
+vim.keymap.set('n', '<leader>k', require('tree-climber').swap_prev, keyopts)
+vim.keymap.set('n', '<leader>j', require('tree-climber').swap_next, keyopts)
+vim.keymap.set('n', '<leader>h', require('tree-climber').highlight_node, keyopts)
