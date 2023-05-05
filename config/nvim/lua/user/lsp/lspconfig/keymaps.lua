53b62f5c42005dec0c1de5dd095078831dc8bb52
Max Tkachenko <makst@users.noreply.github.com>
2023-05-05 09:58:54 +0200

    feat(nvim): use lspconfig keymaps

--- config/nvim/lua/user/lsp/lspconfig/keymaps.lua
+++ config/nvim/lua/user/lsp/lspconfig/keymaps.lua
@@ -3,11 +3,11 @@ local M = {}
 M.setup_keymaps = function(bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gc', vim.lsp.buf.code_action, bufopts)
-  -- vim.keymap.set('n', 'gre', vim.lsp.buf.rename, bufopts)
-  -- vim.keymap.set('n', '<C-j>', vim.diagnostic.goto_next, bufopts)
-  -- vim.keymap.set('n', '<C-k>', vim.diagnostic.goto_prev, bufopts)
-  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  -- vim.keymap.set('n', 'L', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', 'gre', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<C-j>', vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.diagnostic.goto_prev, bufopts)
+  vim.keymap.set('n', '<leader>hd', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'L', vim.lsp.buf.signature_help, bufopts)
 end
 
 return M;
